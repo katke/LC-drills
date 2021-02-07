5f7d748e423d7337cd84fc915a1aac4e52219439
Kat <kmmpatke@gmail.com>
2021-02-07 11:18:25 -0600

    buggy but getting closer

--- src/com/kp/practice/MinimumSubstring.java
+++ src/com/kp/practice/MinimumSubstring.java
@@ -1,6 +1,7 @@
 package com.kp.practice;
 
 import java.util.*;
+
 // 76 https://leetcode.com/problems/minimum-window-substring/
 // Status: brute force, not optimized
 public class MinimumSubstring implements TestCase {
@@ -16,36 +17,44 @@ public class MinimumSubstring implements TestCase {
     var l = 0;
     var r = t.length();
     var result = "";
-//    int tCodepointsTotal = t.codePoints().sum();
-//    Map<Character, Integer> tMap = setTMap(t);
-    List<Character> tList = setTList(t);
-//    System.out.println("tCodepointsTotal: " + tCodepointsTotal);
-//    Set<Integer> tSet = getTCodepointsSet(t);
-    while (r < s.length()) {
-      var currentWindow = s.substring(l, r + 1);
-      var newChar = s.charAt(r);
-      tList.remove((Character) newChar);
-//      System.out.println("Current window: " + currentWindow);
-//      System.out.println("l: " + s.charAt(l) + ", r: " + s.charAt(r));
-//      System.out.println("tList: " + tList.toString());
 
-      if (tList.isEmpty()) {
-//        System.out.println("All chars accounted for");
-        tList = setTList(t);
-        if (result.length() == 0 || currentWindow.length() < result.length()) {
-//          System.out.println("Setting result to new window");
-          result = currentWindow;
+    while (r < s.length()) {
+      while (!allCharsAccountedFor(t, s, l, r)) {
+        r++;
+        System.out.println("Current window: " + s.substring(l, r));
+      }
+      if (result.length() == 0 || (r - l + 1) < result.length()) {
+        System.out.println("'R': Setting result to current window");
+        result = s.substring(l, r);
+      }
+      l++;
+      while (allCharsAccountedFor(t, s, l, r) && l < r) {
+        if (result.length() == 0 || (r - l + 1) < result.length()) { // HEr = 3 | rARRH
+          result = s.substring(l, r);
+          System.out.println("'L': Setting result to current window");
         }
         l++;
-        r = l;
-      } else {
-        r++;
+        System.out.println("Current window: " + s.substring(l, r));
       }
+      System.out.println("Result: " + result);
+      r++;
+
       // keep incrementing r until matching window is found
       // once it is, increment l until the window is no longer desirable
       // then increment r, and repeat
     }
     return result;
+  }
+
+  private boolean allCharsAccountedFor(String t, String s, int left, int right) {
+    var tList = setTList(t);
+    var window = s.substring(left, right);
+    for (var i = 0; i < window.length(); i++) {
+      Character character = window.charAt(i);
+      tList.remove(character);
+      if (tList.isEmpty()) break;
+    }
+    return tList.isEmpty();
   }
 
   private boolean allCharsAccountedFor(Map<Character, Integer> tMap) {
@@ -76,12 +85,55 @@ public class MinimumSubstring implements TestCase {
 
   public Map<String, List<String>> getTestCases() {
     return Map.of(
-        "Expected: BANC", List.of("ADOBECODEBANC", "ABC"),
-        "Expected: \"\"", List.of("ABC", "ABCD"),
-        "Expected: \" \"", List.of("a", "b"),
-        "Expected: a", List.of("a", "a"),
-        "Expected: bCBA", List.of("ABCODEBaNbCBA", "AbC"),
+//        "Expected: BANC", List.of("ADOBECODEBANC", "ABC"),
+//        "Expected: \"\"", List.of("ABC", "ABCD"),
+//        "Expected: \" \"", List.of("a", "b"),
+//        "Expected: a", List.of("a", "a"),
+//        "Expected: bCBA", List.of("ABCODEBaNbCBA", "AbC"),
         "Expected: HEr", List.of("talkeHandHErRzJrARRHNDeoRRRRzalkje", "Hr")
     );
   }
 }
+
+//    System.out.println("tCodepointsTotal: " + tCodepointsTotal);
+//    Set<Integer> tSet = getTCodepointsSet(t);
+//    while (r < s.length()) {
+//      var currentWindow = s.substring(l, r + 1);
+//      var newChar = s.charAt(r);
+//      tList.remove((Character) newChar);
+////      System.out.println("Current window: " + currentWindow);
+////      System.out.println("l: " + s.charAt(l) + ", r: " + s.charAt(r));
+////      System.out.println("tList: " + tList.toString());
+//
+//      if (tList.isEmpty()) {
+////        System.out.println("All chars accounted for");
+//        tList = setTList(t);
+//        if (result.length() == 0 || currentWindow.length() < result.length()) {
+////          System.out.println("Setting result to new window");
+//          result = currentWindow;
+//        }
+//        l++;
+//        r = l;
+//      } else {
+//        r++;
+//      }
+
+//while (r < s.length()) {
+//    tList = setTList(t);
+//    if (result.length() == 0 || currentWindow.length() < result.length()) {
+//    System.out.println("Setting result to current window");
+//    result = currentWindow;
+//    }
+//    l++;
+//    r = l;
+//    while (!tList.isEmpty()) {
+//    currentWindow = s.substring(l, r + 1);
+//    System.out.println("Current window: " + currentWindow);
+//    char newChar = s.charAt(r); // todo
+//    tList.remove((Character) newChar);
+//    System.out.println("tList: " + tList.toString());
+//    r++;
+//    if (r >= s.length()) {
+//    break;
+//    }
+//    }
