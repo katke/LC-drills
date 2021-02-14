38a24f1e62ce2457b8dd0fe0393dfced1aa7c8c5
Kat <kmmpatke@gmail.com>
2021-02-13 18:24:09 -0600

    starting longest substring

---
+++ src/com/kp/practice/LongestSubstringTwoChars.java
@@ -0,0 +1,19 @@
+package com.kp.practice;
+
+import java.util.List;
+import java.util.Map;
+
+// 159 https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/
+public class LongestSubstringTwoChars {
+
+  public int solution(String t) {
+    return 0;
+  }
+
+  public Map<String, List<String>> getTestCases() {
+    return Map.of(
+        "1. Expected 3", List.of("eceba"),
+        "2. Expected 5", List.of("ccaabbb")
+    );
+  }
+}
