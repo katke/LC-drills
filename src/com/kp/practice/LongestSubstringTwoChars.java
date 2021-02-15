973f0a236caa0bb96cb729b169d8651e53c8ed64
Kat <kmmpatke@gmail.com>
2021-02-14 21:26:02 -0600

    partial solution but still buggy

--- src/com/kp/practice/LongestSubstringTwoChars.java
+++ src/com/kp/practice/LongestSubstringTwoChars.java
@@ -8,29 +8,60 @@ import java.util.Map;
 public class LongestSubstringTwoChars implements TestCase {
 
   public int solution(String t) {
-    var tCountMap = tCountMap(t);
+    if (t == null) {
+      return 0;
+    } else if (t.length() < 2) {
+      return t.length();
+    }
+    Map<Character, Integer> tMap = new HashMap<>();
     var l = 0;
-    var r = 0;
-
-    return 0;
+    var r = 1;
+//    var currentWindow = t.substring(l, r);
+    updateCountMap(t.charAt(l), tMap, true);
+    updateCountMap(t.charAt(r), tMap, true);
+    var max = 2;
+    while (r < t.length()) {
+      System.out.println("l: " + l + ", r: " + r);
+      if (tMap.keySet().size() > 2) {
+        // condition is now broken
+        // increment left
+        updateCountMap(t.charAt(l), tMap, false);
+        System.out.println(tMap.toString());
+        l++;
+      } else {
+        // keep incrementing right
+        max = Math.max(max, r - l);
+        updateCountMap(t.charAt(r), tMap, true);
+        System.out.println(tMap.toString());
+        r++;
+      }
+      System.out.println("max: " + max);
+    }
+    return max;
   }
 
-  private Map<Character, Integer> tCountMap(String t) {
-    Map<Character, Integer> tMap = new HashMap<>();
-    for (int i = 0; i < t.length(); i++) {
-      tMap.compute(t.charAt(i), (k, v) -> v == null ? 0 : v + 1);
+  private void updateCountMap(Character c, Map<Character, Integer> tMap, boolean add) {
+    if (add) {
+      tMap.compute(c, (k, v) -> v == null ? 1 : v + 1);
+    } else {
+      var currentVal = tMap.getOrDefault(c, -1);
+      if (currentVal > 1) {
+        tMap.put(c, currentVal - 1);
+      } else {
+        tMap.remove(c);
+      }
     }
-    return tMap;
   }
 
   public Map<String, List<String>> getTestCases() {
     return Map.of(
         "1. Expected 3", List.of("eceba"),
-        "2. Expected 5", List.of("ccaabbb"),
-        "3. Expected 0", List.of(""),
-        "4. Expected 0", List.of("bbbbbbbbbbbbb"),
-        "5. Expected 3", List.of("cccccccCabb"),
-        "6. Expected 10", List.of("yyyyyyyyywyyyyyyyyalksdjfaaaaaaaa")
+        "2. Expected 5", List.of("ccaabbb")
+//        "3. Expected 0", List.of(""),
+//        "4. Expected 0", List.of("bbbbbbbbbbbbb"),
+//        "5. Expected 3", List.of("cccccccCabb"),
+//        "6. Expected 10", List.of("yyyyyyyyywyyyyyyyyalksdjfaaaaaaaa"),
+//        "7. Expected 1", List.of("y")
     );
   }
 }
