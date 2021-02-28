68cb617e6c3afade393fa79d53cbb6721132f505
Kat <kmmpatke@gmail.com>
2021-02-27 22:00:42 -0600

    max substring with two chars solution accepted

--- src/com/kp/practice/LongestSubstringTwoChars.java
+++ src/com/kp/practice/LongestSubstringTwoChars.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 // 159 https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/
+// Status: Accepted!
 public class LongestSubstringTwoChars implements TestCase {
 
   public int solution(String t) {
@@ -16,26 +17,22 @@ public class LongestSubstringTwoChars implements TestCase {
     Map<Character, Integer> tMap = new HashMap<>();
     var l = 0;
     var r = 1;
-//    var currentWindow = t.substring(l, r);
     updateCountMap(t.charAt(l), tMap, true);
-    updateCountMap(t.charAt(r), tMap, true);
     var max = 2;
-    while (r < t.length()) {
-      System.out.println("l: " + l + ", r: " + r);
+    while (r <= t.length()) {
       if (tMap.keySet().size() > 2) {
         // condition is now broken
         // increment left
         updateCountMap(t.charAt(l), tMap, false);
-        System.out.println(tMap.toString());
         l++;
       } else {
         // keep incrementing right
-        max = Math.max(max, r - l);
+        var possibleNewMax = tMap.values().stream().reduce(0, (subtotal, next) -> subtotal + next);
+        max = Math.max(max, possibleNewMax);
+        if (r == t.length()) break;
         updateCountMap(t.charAt(r), tMap, true);
-        System.out.println(tMap.toString());
         r++;
       }
-      System.out.println("max: " + max);
     }
     return max;
   }
@@ -56,12 +53,13 @@ public class LongestSubstringTwoChars implements TestCase {
   public Map<String, List<String>> getTestCases() {
     return Map.of(
         "1. Expected 3", List.of("eceba"),
-        "2. Expected 5", List.of("ccaabbb")
-//        "3. Expected 0", List.of(""),
-//        "4. Expected 0", List.of("bbbbbbbbbbbbb"),
-//        "5. Expected 3", List.of("cccccccCabb"),
-//        "6. Expected 10", List.of("yyyyyyyyywyyyyyyyyalksdjfaaaaaaaa"),
-//        "7. Expected 1", List.of("y")
+        "2. Expected 5", List.of("ccaabbb"),
+        "3. Expected 0", List.of(""),
+        "4. Expected 13", List.of("bbbbbbbbbbbbb"),
+        "5. Expected 9", List.of("cccccccCcabbbbbbBbb"),
+        "6. Expected 18", List.of("yyyyyyyyywyyyyyyyyalksdjfaaaaaaaa"),
+        "7. Expected 1", List.of("y"),
+        "8. Expected 7", List.of("yyyyaaab")
     );
   }
 }
