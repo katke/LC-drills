31c2b17f40d6f0d7d23d0254075cc1de9d3993aa
Kat <kmmpatke@gmail.com>
2021-03-13 21:45:51 -0600

    aww yisss solution accepted

--- src/com/kp/practice/AllPathsSourceToTarget.java
+++ src/com/kp/practice/AllPathsSourceToTarget.java
@@ -1,46 +1,53 @@
 package com.kp.practice;
 
 // 797 https://leetcode.com/problems/all-paths-from-source-to-target/
+// status: Accepted! (and faster than 95% of the other submissions)
 import java.util.*;
 
 public class AllPathsSourceToTarget implements TestCase {
 
   public List<List<Integer>> solution(int[][] graph) {
     var allPaths = new ArrayList<List<Integer>>();
-    findPaths(allPaths, new ArrayList<Integer>(), 0, graph);
-
+    findPaths(allPaths, new ArrayList<>(), 0, graph);
     return allPaths;
   }
 
-  //[ [4,3,1], [3,2,4], [3], [4], [] ]
-
-  private void findPaths(List<List<Integer>> allPaths, List<Integer> currentPath, int nextNode, final int[][] graph) {
+  private void findPaths(List<List<Integer>> allPaths, List<Integer> currentPath, int currentNode, final int[][] graph) {
+    currentPath.add(currentNode);
     var target = graph.length - 1;
-    if (currentPath == null) {
-      currentPath = new ArrayList<>();
-    }
-    if (nextNode == target) {
-      currentPath.add(nextNode);
+    if (currentNode == target) {
       allPaths.add(currentPath);
       return;
     }
-    currentPath.add(nextNode);
-    var adjList = graph[nextNode];
-    findPaths(allPaths, currentPath, nextNode, graph);
-
-
+    var adjList = graph[currentNode];
+    if (adjList.length == 0) return;
+    for (int nextNode : adjList) {
+      findPaths(allPaths, new ArrayList<>(currentPath), nextNode, graph);
     }
   }
 
   @Override
   public Map<String, int[][]> getTestCases() {
     return Map.of(
-//        "1. Expected 2 paths, [[0,1,3],[0,2,3]]", lcExample1(),
-        "2. Expected 5 paths, [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]", lcExample2()
-//        "3. Expected 1 path, [[0,1]]", lcExample3(),
-//        "4. Expected 3 paths, [[0,1,2,3],[0,2,3],[0,3]]", lcExample4(),
-//        "5. Expected 2 paths, [[0,1,2,3],[0,3]]", lcExample5()
+        "1. Expected 2 paths, [[0,1,3],[0,2,3]]", lcExample1(),
+        "2. Expected 5 paths, [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]", lcExample2(),
+        "3. Expected 1 path, [[0,1]]", lcExample3(),
+        "4. Expected 3 paths, [[0,1,2,3],[0,2,3],[0,3]]", lcExample4(),
+        "5. Expected 2 paths, [[0,1,2,3],[0,3]]", lcExample5(),
+        "6. Expected 1 path, [[0,1,3,5,6]]", graphWithNonTargetDeadend()
     );
+  }
+
+  private int[][] graphWithNonTargetDeadend() {
+    return new int[][] {
+        {1, 2},
+        {3},
+        {4},
+        {5},
+        {},
+        {6},
+        {}
+    };
   }
 
   private int[][] lcExample5() {
