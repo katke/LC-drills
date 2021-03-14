13722422cc2fc1eda0ff47a9ff8862499e8e0e1b
Kat <kmmpatke@gmail.com>
2021-03-13 18:52:30 -0600

    nope somehow made it work even less well

--- src/com/kp/practice/AllPathsSourceToTarget.java
+++ src/com/kp/practice/AllPathsSourceToTarget.java
@@ -6,39 +6,40 @@ import java.util.*;
 public class AllPathsSourceToTarget implements TestCase {
 
   public List<List<Integer>> solution(int[][] graph) {
-    var startingPoints = graph[0];
     var allPaths = new ArrayList<List<Integer>>();
-    for (int node : startingPoints) {
-      var path = new ArrayList<Integer>();
-      path.add(0);
-      allPaths.add(findPath(path, node, graph));
-    }
+    findPaths(allPaths, new ArrayList<Integer>(), 0, graph);
+
     return allPaths;
   }
 
-  private List<Integer> findPath(List<Integer> currentPath, int nextNode, int[][] graph) {
-    currentPath.add(nextNode);
-    var target = graph.length;
-    var adjList = graph[nextNode];
-    for (int nextNodeToCheck : adjList) {
-      if (nextNodeToCheck == target) {
-        currentPath.add(nextNodeToCheck);
-        return currentPath;
-      } else {
-        return findPath(currentPath, nextNodeToCheck, graph);
-      }
+  //[ [4,3,1], [3,2,4], [3], [4], [] ]
+
+  private void findPaths(List<List<Integer>> allPaths, List<Integer> currentPath, int nextNode, final int[][] graph) {
+    var target = graph.length - 1;
+    if (currentPath == null) {
+      currentPath = new ArrayList<>();
     }
-    return currentPath;
+    if (nextNode == target) {
+      currentPath.add(nextNode);
+      allPaths.add(currentPath);
+      return;
+    }
+    currentPath.add(nextNode);
+    var adjList = graph[nextNode];
+    findPaths(allPaths, currentPath, nextNode, graph);
+
+
+    }
   }
 
   @Override
   public Map<String, int[][]> getTestCases() {
     return Map.of(
-        "1. Expected 2 paths, [[0,1,3],[0,2,3]]", lcExample1(),
-        "2. Expected 5 paths, [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]", lcExample2(),
-        "3. Expected 1 path, [[0,1]]", lcExample3(),
-        "4. Expected 3 paths, [[0,1,2,3],[0,2,3],[0,3]]", lcExample4(),
-        "5. Expected 2 paths, [[0,1,2,3],[0,3]]", lcExample5()
+//        "1. Expected 2 paths, [[0,1,3],[0,2,3]]", lcExample1(),
+        "2. Expected 5 paths, [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]", lcExample2()
+//        "3. Expected 1 path, [[0,1]]", lcExample3(),
+//        "4. Expected 3 paths, [[0,1,2,3],[0,2,3],[0,3]]", lcExample4(),
+//        "5. Expected 2 paths, [[0,1,2,3],[0,3]]", lcExample5()
     );
   }
 
