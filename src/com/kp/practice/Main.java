d1d8f7ca365b340c9cbccabdd36b8c557bdf7735
Kat <kmmpatke@gmail.com>
2021-04-18 11:49:43 -0500

    first pass working with simple test case

--- src/com/kp/practice/Main.java
+++ src/com/kp/practice/Main.java
@@ -10,13 +10,13 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        var lcClass = new MaximumDepthBinaryTree();
-        for (Map.Entry<String, TreeNode> testCase : lcClass.getTestCases().entrySet()) {
+        var lcClass = new MergeTwoSortedArrays();
+        for (Map.Entry<String, List<ListNode>> testCase : lcClass.getTestCases().entrySet()) {
             var description = testCase.getKey();
             var params = testCase.getValue();
             System.out.println("-------------------");
-            var actual = lcClass.solution(params);
-            System.out.println(description + ", actual: " + actual);
+            var actual = lcClass.solution(params.get(0), params.get(1));
+            System.out.println(description + ", actual: " + actual.toStringRepresentation());
         }
 
     }
--- src/com/kp/practice/MergeTwoSortedArrays.java
+++ src/com/kp/practice/MergeTwoSortedArrays.java
@@ -7,8 +7,37 @@ import java.util.Map;
 
 // 21 https://leetcode.com/problems/merge-two-sorted-lists/
 public class MergeTwoSortedArrays implements TestCase {
-  public void solution(ListNode l1, ListNode l2) {
+  public ListNode solution(ListNode l1, ListNode l2) {
+    if (l1 == null) {
+      return l2;
+    } else if (l2 == null) {
+      return l1;
+    }
+    ListNode merged = l1;
+    var l2Next = l2;
+    var currentMergedNode = merged;
+    while (l2Next != null) {
+      // save existing currentMergedNode to refer back to once we find the insertion point
+      var prevMergedNode = currentMergedNode;
+      while (currentMergedNode != null && currentMergedNode.val < l2Next.val) {
+        // traverse merged until we find the point where currentl2Val is greater than
+        // currentMergedNode value
+        prevMergedNode = currentMergedNode;
+        currentMergedNode = currentMergedNode.next;
+      }
+      mergeInNewNode(prevMergedNode, l2Next);
+      l2Next = l2Next.next;
+    }
+    return merged;
+  }
 
+  private void mergeInNewNode(ListNode mergedInsertionPoint, ListNode l2Node) {
+    // extract the current mergedInsertionPoint.next
+    var existingNextNode = mergedInsertionPoint.next;
+    // point mergedInsertionPoint.next to a node with l2 val
+    // point that new node next to the prev mergedInsertionPoint.next
+    // set mergedInsertionPoint to mergedInsertionPoint.next
+    mergedInsertionPoint.next = new ListNode(l2Node.val, existingNextNode);;
   }
 
   @Override
