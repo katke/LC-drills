42b7037d3749a07ef5038a190b55ecdee0ec9cee
Kat <kmmpatke@gmail.com>
2021-05-02 15:03:46 -0500

    median of two sorted arrays accepted

--- src/com/kp/practice/Main.java
+++ src/com/kp/practice/Main.java
@@ -9,12 +9,12 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        var lcClass = new MaxProfit();
+        var lcClass = new MedianTwoSortedArrays();
         for (Map.Entry<String, ?> testCase : lcClass.getTestCases().entrySet()) {
             var description = testCase.getKey();
             var params = (List<int[]>) testCase.getValue();
             System.out.println("-------------------");
-            var actual = lcClass.solution(params.get(0), params.get(1), params.get(2));
+            var actual = lcClass.solution(params.get(0), params.get(1));
             System.out.println(description + ", actual: " + actual);
         }
 
